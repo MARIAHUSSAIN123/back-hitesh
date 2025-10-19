ce6f8b95f958240376dd6ccf96b4e76b00b56e0e
Maria Hussain <162424488+MARIAHUSSAIN123@users.noreply.github.com>
2025-10-19 23:37:40 +0500

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 
-const PORT = process.env.PORT || 3000   // ✅ Correct PORT handling
+const PORT = 3000   // ✅ Correct PORT handling
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
@@ -12,6 +12,6 @@ app.get('/youtube', (req, res) => {
   res.send('Hello World! welcome to youtube')
 })
 
-app.listen(PORT, () => {
+app.listen(process.env.PORT, () => {
   console.log(`Server is running on port ${PORT}`)  // ✅ Correct log
 })
